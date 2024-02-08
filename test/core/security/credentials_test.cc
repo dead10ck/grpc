6a1a9d4f729f7b9ad3d2c3ab43d4a310c0ea09c3
Tanvi Jagtap <tjagtap@google.com>
2024-02-07 21:38:22 -0800

    [export] Redacting unknown metadata types in debug logs.
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/605195843](http://cl/605195843)
    
    PiperOrigin-RevId: 605195843

--- test/core/security/credentials_test.cc
+++ test/core/security/credentials_test.cc
@@ -511,8 +511,9 @@ class RequestMetadataState : public RefCounted<RequestMetadataState> {
     md_.Remove(HttpAuthorityMetadata());
     md_.Remove(HttpPathMetadata());
     gpr_log(GPR_INFO, "expected metadata: %s", expected_.c_str());
-    gpr_log(GPR_INFO, "actual metadata: %s", md_.DebugString().c_str());
-    GPR_ASSERT(md_.DebugString() == expected_);
+    gpr_log(GPR_INFO, "actual metadata: %s",
+            md_.TestOnlyDebugStringUnredacted().c_str());
+    GPR_ASSERT(md_.TestOnlyDebugStringUnredacted() == expected_);
   }
 
   grpc_error_handle expected_error_;
